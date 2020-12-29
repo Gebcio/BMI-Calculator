84bfd1d8266bd70019906241c761b205eed863d1
unknown <rafals.gebauer@gmail.com>
2020-12-29 14:48:56 +0100

    add calculateMetric function && analyzeBMI function

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ function childAdultSwitch() {
   adult.classList.toggle("hidden");
   child.classList.toggle("hidden");
 }
+
+function calculateMetric() {
+  const heightCentimeters = document.getElementById("heightCm").value;
+  const weight = document.getElementById("weightKg").value;
+  const heightMeters = heightCentimeters / 100;
+
+  const metricBMI =
+    Math.round((weight / (heightMeters * heightMeters)) * 10) / 10;
+
+  analyzeBMI(metricBMI);
+}
+
+function analyzeBMI(valueOfBMI) {
+  if (valueOfBMI < 18.4) {
+    alert(`Your BMI is: ${valueOfBMI}. This is too low`);
+  } else if (valueOfBMI > 18.5 && valueOfBMI < 24.9) {
+    alert(`Your BMI is: ${valueOfBMI}. This is correct weight`);
+  } else if (valueOfBMI > 25.0 && valueOfBMI < 29.9) {
+    alert(`Your BMI is: ${valueOfBMI}. This is too much`);
+  } else if (valueOfBMI > 30.0 && valueOfBMI < 34.9) {
+    alert(`Your BMI is: ${valueOfBMI}. This is way too much. Stage I`);
+  } else if (valueOfBMI > 35.0 && valueOfBMI < 39.9) {
+    alert(`Your BMI is: ${valueOfBMI}. This is way way too much. Stage II`);
+  } else {
+    alert(`Your BMI is: ${valueOfBMI}. This is insane`);
+  }
+}
