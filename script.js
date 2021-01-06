aafd63cf3bdb30d90273e33a3d478973b202112c
Gebcio <rafals.gebauer@gmail.com>
2021-01-06 18:08:27 +0100

    add imperialBMI calculation

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-tabButtonActiveSwitch = () => {
+"use strict";
+
+const calculateBtn = document.getElementById("calculateButton");
+
+function switchChildAdult() {
+  const adult = document.querySelector(".ageAdultBox");
+  const child = document.querySelector(".ageChildBox");
   const tabButtonAdult = document.querySelector(".tabButtonAdult");
   const tabButtonChild = document.querySelector(".tabButtonChild");
 
+  adult.classList.toggle("hidden");
+  child.classList.toggle("hidden");
   tabButtonAdult.classList.toggle("active");
   tabButtonChild.classList.toggle("active");
-};
+  calculateBtn.classList.toggle("child");
+}
 
 function switchSystemOfUnits() {
   const imperial = document.querySelector(".imperial");
@@ -12,25 +21,7 @@ function switchSystemOfUnits() {
 
   imperial.classList.toggle("hidden");
   metric.classList.toggle("hidden");
-}
-
-function childAdultSwitch() {
-  const adult = document.querySelector(".ageAdultBox");
-  const child = document.querySelector(".ageChildBox");
-
-  adult.classList.toggle("hidden");
-  child.classList.toggle("hidden");
-}
-
-function calculateMetric() {
-  const heightCentimeters = document.getElementById("heightCm").value;
-  const weight = document.getElementById("weightKg").value;
-  const heightMeters = heightCentimeters / 100;
-
-  const metricBMI =
-    Math.round((weight / (heightMeters * heightMeters)) * 10) / 10;
-
-  analyzeBMI(metricBMI);
+  calculateBtn.classList.toggle("metric");
 }
 
 function analyzeBMI(valueOfBMI) {
@@ -46,5 +37,32 @@ function analyzeBMI(valueOfBMI) {
     alert(`Your BMI is: ${valueOfBMI}. This is way way too much. Stage II`);
   } else {
     alert(`Your BMI is: ${valueOfBMI}. This is insane`);
+  }
+}
+
+function calculateBMI() {
+  if (!calculateBtn.classList.contains("metric")) {
+    const heightFeet = Number(document.getElementById("heightFt").value);
+    const heightInches = Number(document.getElementById("heightInch").value);
+    const weightStone = Number(document.getElementById("weightSt").value);
+    const weightPound = Number(document.getElementById("weightLbs").value);
+
+    const heightInInches = heightFeet * 12 + heightInches;
+    const weightInLbs = weightStone * 14 + weightPound;
+
+    const imperialBMI =
+      Math.round(703 * (weightInLbs / (heightInInches * heightInInches)) * 10) /
+      10;
+
+    analyzeBMI(imperialBMI);
+  } else {
+    const heightCentimeters = Number(document.getElementById("heightCm").value);
+    const weightKg = Number(document.getElementById("weightKg").value);
+    const heightMeters = heightCentimeters / 100;
+
+    const metricBMI =
+      Math.round((weightKg / (heightMeters * heightMeters)) * 10) / 10;
+
+    analyzeBMI(metricBMI);
   }
 }
